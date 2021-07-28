9a08f03d5e13671cfb4c2ea8a804ca2744ff7872
Sean Chen <seanchen11235@gmail.com>
2021-07-28 09:59:57 -0500

    Add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Trial and Error
+
+## An experimental crate for proof-of-concept proposals from the Error Handling Project Group
+
+This crate contains the following experimental modules:
+
+1. An alternative to `Box<dyn Error + ...>` that implements the `Error` trait.
+2. An error reporter that wraps an error and handles iterating over sources and formatting of error reports.
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-//! An experiment crate for proposals from the error handling project group.
+//! An experimental crate for proposals from the error handling project group.
 //!
 //! This crate currently contains two experiments, each in their own module.
 //!
 //! 1. An alternative to `Box<dyn Error + ...>` that implements `Error`.
-//! 1. An error reporter that wraps an error and handles iterating over sources
+//! 2. An error reporter that wraps an error and handles iterating over sources
 //!    and formatting a full error report.
 //!
 #![feature(try_trait_v2)]
