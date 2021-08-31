77ed946da74961a09d8a52a19862084e9dc9266f
Sean Chen <seanchen11235@gmail.com>
2021-08-31 08:10:13 -0500

    Update src/error_reporter.rs
    Co-authored-by: Jane Lusby <jlusby42@gmail.com>

--- src/error_reporter.rs
+++ src/error_reporter.rs
@@ -11,7 +11,7 @@
 //! as a single line, or in a multi-line format with each cause in the error chain on a new line).
 //!
 //! Note that this `Report` type, which we'll refer to as `std::Report`, is not meant as a
-//! replacement for `eyre::Report`. `std::Report` is much more minimal in comparison; its only job
+//! replacement for types like `eyre::Report` or `anyhow::Error`. `std::Report` is much more minimal in comparison; its only job
 //! is to expose an interface for formatting errors that you want to print. `eyre::Report` is able
 //! to additionally store an error and additional context. It also supports custom user-defined
 //! output formats, while `std::Report` only makes available a single formatting option that is
